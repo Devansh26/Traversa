3ee214ec32463dfa568a2a1f20e3471bc48b61b7
Devansh26 <devansh981226@gmail.com>
2022-04-07 13:42:11 +0530

    Add files via upload

---
+++ login/index.php
@@ -0,0 +1,42 @@
+<?php
+  session_start();
+?>
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>Pure CSS Login Form</title>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript"></script>
+
+
+  
+  
+      <link rel="stylesheet" href="css/style.css">
+
+  
+</head>
+
+<body>
+
+  <h1>Flat Login Form inside a Mac Monitor</h1>
+
+<div class="stand">
+  <div class="outer-screen">
+    <div class="inner-screen">
+      <div class="form">
+        <input type="text" class="zocial-dribbble" placeholder="Enter your email" />
+        <input type="text" placeholder="Password" />
+         <input type="submit" value="Login" />
+        <a href="">Lost your password?</a>
+      </div> 
+    </div> 
+  </div> 
+</div>
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+  
+
+</body>
+
+</html>
